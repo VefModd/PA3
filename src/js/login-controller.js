78e66a72dbbd7ac83ca9bb77424e19e217eb34ec
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-03-06 16:56:39 +0000

    login form validation done

--- src/js/login-controller.js
+++ src/js/login-controller.js
@@ -1,18 +1,20 @@
 angular.module('angularEvaluation').controller('LoginController', ['$scope', '$routeParams', '$route', 'dispatch',
         function ($scope, $routeParams, $route, dispatch) {
             $scope.login = function() {
-                dispatch.login($scope.username, $scope.password).
-                    success(function(data, status, headers, config) {
-                        console.log("success! data => ", data);
-                        console.log("success! status=> ", status);
-                        console.log("success! headers=> ", headers);
-                        console.log("success! config=> ", config);
-                    }).
-                    error(function(data, status, headers, config) {
-                        console.log("error! data=> ", data);
-                        console.log("error! status=> ", status);
-                        console.log("error! headers=> ", headers);
-                        console.log("error! config=> ", config);
-                    });
+                if($scope.loginForm.$valid) {
+                    dispatch.login($scope.user.name, $scope.user.pass).
+                        success(function(data, status, headers, config) {
+                            console.log("success! data => ", data);
+                            console.log("success! status=> ", status);
+                            console.log("success! headers=> ", headers);
+                            console.log("success! config=> ", config);
+                        }).
+                        error(function(data, status, headers, config) {
+                            console.log("error! data=> ", data);
+                            console.log("error! status=> ", status);
+                            console.log("error! headers=> ", headers);
+                            console.log("error! config=> ", config);
+                        });
+                }
             }
         }]);

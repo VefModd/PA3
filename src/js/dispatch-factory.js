7f1224f83bf8d9cee4bf0b90d4473baae3448c8a
danben4 <danielbenediktsson4@gmail.com>
2015-03-06 15:19:00 +0000

    yes

--- src/js/dispatch-factory.js
+++ src/js/dispatch-factory.js
@@ -1,4 +1,4 @@
-angular.module("angularEvaluation").constant("SERVER_URL", "http://dispatch.ru.is/demo/api/v1/");
+angular.module("angularEvaluation").constant("SERVER_URL", "http://dispatch.ru.is/h09/api/v1/");
 
 angular.module("angularEvaluation").factory("dispatch", 
     function($http, SERVER_URL) {

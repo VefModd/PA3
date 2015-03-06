b8f0f47f0a0ce3c19be222ec5c942896d9cf2636
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-03-06 19:11:00 +0000

    login fail message

--- src/js/login-controller.js
+++ src/js/login-controller.js
@@ -1,13 +1,14 @@
 angular.module('angularEvaluation').controller('LoginController', ['$scope', '$routeParams', '$route', '$location', 'dispatch',
         function ($scope, $routeParams, $route, $location, dispatch) {
+            $scope.loginFail = false;
             $scope.login = function() {
                 if($scope.loginForm.$valid) {
                     dispatch.login($scope.user.name, $scope.user.pass).
                         success(function(data, status, headers, config) {
-                            console.log("success! data => ", data);
-                            console.log("success! status=> ", status);
-                            console.log("success! headers=> ", headers);
-                            console.log("success! config=> ", config);
+                            // console.log("success! data => ", data);
+                            // console.log("success! status=> ", status);
+                            // console.log("success! headers=> ", headers);
+                            // console.log("success! config=> ", config);
                             if(data.User.Role == 'student') {
                                 $location.path('/front-page-student/' + $scope.username);
                             }
@@ -16,10 +17,11 @@ angular.module('angularEvaluation').controller('LoginController', ['$scope', '$r
                             }
                         }).
                         error(function(data, status, headers, config) {
-                            console.log("error! data=> ", data);
-                            console.log("error! status=> ", status);
-                            console.log("error! headers=> ", headers);
-                            console.log("error! config=> ", config);
+                            // console.log("error! data=> ", data);
+                            // console.log("error! status=> ", status);
+                            // console.log("error! headers=> ", headers);
+                            // console.log("error! config=> ", config);
+                            $scope.loginFail = true;
                         });
                 }
             }

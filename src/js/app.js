ab13e9f5408ea86e915d655ce041c3ab95c6358c
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-03-06 14:20:37 +0000

    fixed angular version issue

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,12 @@
-angular.module('angularEvaluation', [
-        'ngRoute',
-        'ui.bootstrap'
-        ]);
-
-angular.module('angularEvaluation').config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.
-                when('/login', {
-                    templateUrl: 'src/html/login.html',
-                    controller: 'LoginController'
-                }).
-                otherwise({
-                    redirectTo: '/login'
-                });
-        }]);
+angular.module('angularEvaluation', [ 'ngRoute', 'ui.bootstrap' ]).
+    config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: 'src/html/login.html',
+                controller: 'LoginController'
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }]);

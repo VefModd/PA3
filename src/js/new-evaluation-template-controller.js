4f9f26a212ee2b4f91c65baa2a0add2793fea6cb
danben4 <danielbenediktsson4@gmail.com>
2015-03-12 14:08:18 +0000

    Working on evaluation template

--- src/js/new-evaluation-template-controller.js
+++ src/js/new-evaluation-template-controller.js
@@ -1,4 +1,19 @@
 angular.module('angularEvaluation').controller('NewEvaluationController', ['$scope', '$routeParams', '$route', 'dispatch',
     function($scope, $routeParams, $route, dispatch) {
-        console.log("This is great!");
+        console.log("INSDIE NEW EVALUATION CONTROLLER!");
+        $scope.evaluationTemplate = {
+            "ID": 0,
+            "Title": "",
+            "TitleEN": "",
+            "IntroText": "",
+            "IntroTextEN": "",
+            "CourseQuestions": [],
+            "TeacherQuestions": []
+        };
+
+        console.log("evaluationTemplate before adding anything to is: ", $scope.evaluationTemplate);
+
+        $scope.test = function() {
+            console.log("evaluationTemplate after adding something to it: ", $scope.evaluationTemplate);
+        };
     }]);

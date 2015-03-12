7c068cdab36b0ad04807c436efa3b43af029c195
danben4 <danielbenediktsson4@gmail.com>
2015-03-12 22:15:42 +0000

    hot

--- src/js/modal-course-question-controller.js
+++ src/js/modal-course-question-controller.js
@@ -1,15 +1,37 @@
 angular.module('angularEvaluation').controller('ModalCourseQuestionController', ['$scope', '$modalInstance',
     function ($scope, $modalInstance) {
-        console.log("inside modal controller!");
+        $scope.question = {
+            "ID": 0,
+            "Text": "",
+            "TextEN": "",
+            "ImageURL": null,
+            "Type": "",
+            "Answers": []
+        };
+
+        $scope.showInputAnswer = false;
+
         console.log("the modal instance: ", $modalInstance);
+        console.log("showInputAnswer: ", $scope.showInputAnswer);
 
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');
         };
 
         $scope.finish = function () {
-            $modalInstance.close();
+            $modalInstance.close($scope.question);
         };
-        
+
+        $scope.inputAnswer = function() {
+            $scope.showInputAnswer = true;
+        };
+
+        $scope.addAnswer = function(answer) {
+            console.log("answer: ", answer);
+            $scope.showInputAnswer = false;
+            $scope.question.Answers.push(answer);
+            console.log("question: ", $scope.question);
+            console.log("question.Answer: ", $scope.question.Answers);
+        };
 
     }]);

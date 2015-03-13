0e1a2ae65a35b1b1d55bcdb66bdec76026e7413b
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-03-13 12:53:08 +0000

    finished login code coverage

--- src/js/login-controller.js
+++ src/js/login-controller.js
@@ -2,17 +2,20 @@ angular.module('angularEvaluation').controller('LoginController', ['$scope', 'di
         function ($scope, dispatch, $rootScope, $routeParams, $location) {
             $scope.login = function() {
                 if($scope.loginForm.$valid) {
+                    console.log("Valid!");
                     dispatch.login($scope.user.name, $scope.user.pass).
                         success(function(data) {
+                            console.log(data.User.Role);
                             $rootScope.data = data;
                             if(data.User.Role === 'student') {
                                 $location.path('/front-page-student');
                             }
-                            else {
+                            if (data.User.Role === 'teacher' || data.User.Role === 'admin'){
                                 $location.path('/front-page-teacher');
                             }
                         }).
                         error(function() {
+                            console.log("error");
                             $scope.loginFail = true;
                         });
                 }

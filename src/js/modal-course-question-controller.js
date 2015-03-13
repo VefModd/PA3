18af7330a9f54e9abeaa44790553c7010c295f75
danben4 <danielbenediktsson4@gmail.com>
2015-03-13 14:59:40 +0000

    flott

--- src/js/modal-course-question-controller.js
+++ src/js/modal-course-question-controller.js
@@ -1,5 +1,7 @@
-angular.module('angularEvaluation').controller('ModalCourseQuestionController', ['$scope', '$modalInstance',
-    function ($scope, $modalInstance) {
+angular.module('angularEvaluation').controller('ModalCourseQuestionController', ['$scope', '$modalInstance', 'typeOfQuestion',
+    function ($scope, $modalInstance, typeOfQuestion) {
+        $scope.typeOfQuestion = typeOfQuestion;
+        $scope.showInputAnswer = false;
         $scope.question = {
             //"ID": 0,
             "Text": "",
@@ -9,14 +11,33 @@ angular.module('angularEvaluation').controller('ModalCourseQuestionController', 
             "Answers": []
         };
 
-        $scope.showInputAnswer = false;
 
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');
         };
 
         $scope.finish = function () {
-            $modalInstance.close($scope.question);
+            console.log("inside finish");
+            if($scope.question.Type !== 'text' &&
+                    $scope.question.Type !== 'single' &&
+                    $scope.question.Type !== 'multiple') {
+                $scope.newCourseQuestionForm.questionType.$invalid = true;
+                $scope.newCourseQuestionForm.$valid = false;
+            }
+            else if(($scope.question.Type === 'single' || $scope.question.Type === 'multiple') &&
+                $scope.question.Answers.length === 0) {
+                $scope.newCourseQuestionForm.questionAnswers.$invalid = true;
+                $scope.newCourseQuestionForm.$valid = false;
+            }
+            else {
+                $scope.newCourseQuestionForm.questionType.$invalid = false;
+                $scope.newCourseQuestionForm.questionAnswers.$invalid = false;
+                $scope.newCourseQuestionForm.$valid = true;
+            }
+
+            if($scope.newCourseQuestionForm.$valid) {
+                $modalInstance.close($scope.question);
+            }
         };
 
         $scope.inputAnswer = function() {
@@ -24,9 +45,13 @@ angular.module('angularEvaluation').controller('ModalCourseQuestionController', 
         };
 
         $scope.addAnswer = function() {
-            $scope.showInputAnswer = false;
-            $scope.question.Answers.push($scope.answer);
-            $scope.answer = "";
+            if($scope.answerForm.$valid) {
+                $scope.showInputAnswer = false;
+                $scope.question.Answers.push($scope.answer);
+                $scope.answer = "";
+                $scope.answerForm.$submitted = false;
+                $scope.newCourseQuestionForm.questionAnswers.$invalid = false;
+            }
         };
 
     }]);

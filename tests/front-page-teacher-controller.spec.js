a65f15d4f309d81de14cc6979d2abf01826c97ee
Jorundur <jorundurj@gmail.com>
2015-03-13 14:48:35 +0000

    a bit more in teacher controller testing

--- tests/front-page-teacher-controller.spec.js
+++ tests/front-page-teacher-controller.spec.js
@@ -1,7 +1,26 @@
 describe('FrontPageTeacherController', function(){
+    beforeEach(module('angularEvaluation'));
 
+    var $controller;
 
+    beforeEach(inject(function (_$controller_){
+        $controller = _$controller_;
+    }));
 
+    describe('$scope.newEvaluationTemplate', function(){
+        var $location;
+        beforeEach(function(){
+            //constructing a fake environment
+            $location = {
+                path : function(p) {
+                    return p;
+                }
+            }
+            spyOn($location, 'path');
+
+        });
+
+    });
 
 
 

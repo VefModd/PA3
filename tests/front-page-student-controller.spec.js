20ad46c3a379ea9f1e1d2704772758086cee3f51
Jorundur <jorundurj@gmail.com>
2015-03-14 12:52:35 +0000

    testingtesting

--- tests/front-page-student-controller.spec.js
+++ tests/front-page-student-controller.spec.js
@@ -7,13 +7,31 @@ describe('FrontPageStudentController', function(){
         myCourses: function() {
             return {
                 success: function(fn){
+                    var fakeCourses = {
+                        data: 'fakeData'
+                    };
+                    fn(fakeCourses);
                     return {
                         error: function(errorFn) {
                         }
                     };
                 }
-            }; 
-       }
+            };
+        },
+        myEvaluations: function(){
+            return {
+                success: function(fn){
+                    var fakeEvals = {
+                        data: 'fakeEval'
+                    };
+                    fn(fakeEvals);
+                    return {
+                        error: function(errorFn){
+                        }
+                    };
+                }
+            };
+        }
     };
 
     beforeEach(inject(function (_$controller_) {

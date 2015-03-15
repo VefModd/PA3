2441bc158848a41bc90627997b591c77fc8c2380
danben4 <danielbenediktsson4@gmail.com>
2015-03-15 14:06:17 +0000

    flott

--- src/js/modal-answer-controller.js
+++ src/js/modal-answer-controller.js
@@ -1,11 +1,14 @@
-angular.module('angularEvaluation').controller('ModalAnswerController', ['$scope', '$modalInstance', 'evaluation',
-    function ($scope, $modalInstance, evaluation) {
+angular.module('angularEvaluation').controller('ModalAnswerController', ['$scope', '$modalInstance', 'evaluation', 'courseName', 'semester',
+    function ($scope, $modalInstance, evaluation, courseName, semester) {
         $scope.answer = {
             "a": undefined,
             "b": undefined
         };
 
-        console.log("ModalAnswerController. evaluation=> ", evaluation);
+        console.log("ModalAnswerController: evaluation=> ", evaluation);
+        console.log("ModalAnswerController: courseName=> ", courseName);
+        console.log("ModalAnswerController: semester=> ", semester);
+        console.log("ModalAnswerController: evaluation.ID=> ", evaluation.ID);
 
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');

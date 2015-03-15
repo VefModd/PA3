6a5a6ba8def8de0d46b454e033f3c5a15377f25c
danben4 <danielbenediktsson4@gmail.com>
2015-03-15 15:00:11 +0000

    fix

--- src/js/modal-answer-controller.js
+++ src/js/modal-answer-controller.js
@@ -1,5 +1,5 @@
-angular.module('angularEvaluation').controller('ModalAnswerController', ['$scope', '$modalInstance', 'evaluation', 'courseName', 'semester',
-    function ($scope, $modalInstance, evaluation, courseName, semester) {
+angular.module('angularEvaluation').controller('ModalAnswerController', ['$scope', '$modalInstance', 'evaluation', 'courseName', 'courseID', 'semester', 'dispatchModalAnswer',
+    function ($scope, $modalInstance, evaluation, courseName, courseID, semester, dispatchModalAnswer) {
         $scope.answer = {
             "a": undefined,
             "b": undefined
@@ -7,12 +7,21 @@ angular.module('angularEvaluation').controller('ModalAnswerController', ['$scope
 
         console.log("ModalAnswerController: evaluation=> ", evaluation);
         console.log("ModalAnswerController: courseName=> ", courseName);
+        console.log("ModalAnswerController: courseID=> ", courseID);
         console.log("ModalAnswerController: semester=> ", semester);
         console.log("ModalAnswerController: evaluation.ID=> ", evaluation.ID);
         $scope.evaluation = evaluation;
         $scope.courseName = courseName;
+        $scope.courseID = courseID;
         $scope.semester = semester;
 
+        dispatchModalAnswer.getTeachers($scope.courseID, $scope.semester).
+            success(function(data) {
+                console.log("SUCCESS - getTeachers - data: ", data);
+            }).
+            error(function() {
+                console.log("ERROR - getTeachers");
+            });
 
 
         $scope.cancel = function() {

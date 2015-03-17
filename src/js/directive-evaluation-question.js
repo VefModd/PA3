943627fe1d87dcbdf9106a3b799c1370f1cc66fb
danben4 <danielbenediktsson4@gmail.com>
2015-03-17 09:52:33 +0000

    flott

--- src/js/directive-evaluation-question.js
+++ src/js/directive-evaluation-question.js
@@ -6,8 +6,10 @@ angular.module("angularEvaluation").directive("evaluationQuestion", function() {
             question: '=ngModel'
         },
         templateUrl: 'src/html/directive-evaluation-question.html',
-        link: function(scope, element, attributes, answerEvaluationForm) {
-            scope.question.Result = [];
+        link: function(scope, answerEvaluationForm) {
+            console.log("INSIDE DIRECTIVE! => scope.question: ", scope.question);
+            //scope.question.Result = [];
+            //(scope, element, attributes, answerEvaluationForm)
             scope.answerEvaluationForm = answerEvaluationForm;
 
             /*
@@ -19,21 +21,24 @@ angular.module("angularEvaluation").directive("evaluationQuestion", function() {
                 }
             };
             */
-
+            /*
             scope.id = scope.question.ID + "";
 
             if(attributes.typeofquestion === 'teacherQuestion') {
                 scope.teacherid = attributes.teacher;
                 scope.id += attributes.teacher;
             }
+            */
 
+            /*
             scope.question.Result[scope.id] = {
                 QuestionID : scope.question.ID,
                 TeacherSSN : attributes.teacher,
                 Value : []
             };
+            */
 
-            console.log("ID: ", scope.id);
+            //console.log("ID: ", scope.id);
 
             /*
             if(scope.question.Type === 'multiple') {
@@ -41,32 +46,26 @@ angular.module("angularEvaluation").directive("evaluationQuestion", function() {
             }
             */
 
-            scope.updateValue = function(answer, id) {
+            scope.updateValue = function(answer, question) {
                 if(answer.checked) {
-                    console.log("questoin: ", scope.question);
-                    scope.question.Result[id].Value.push(answer.Text);
-                    console.log("HEHE");
+                    question.answers.push(answer.Text);
                 } else {
-                    console.log("HERE?");
                     var index = 0;
-                    while(answer.Text !== scope.question.Result[id].Value[index]) {
+                    while(answer.Text !== question.answers[index]) {
                         index++;
                     }
-                    scope.question.Result[id].Value.splice(index, 1);
-                    console.log("HOT");
+                    question.answers.splice(index, 1);
                 }
-                //console.log("inside updateValue: ", scope.question.Result[id].Value);
-
+                console.log("inside updateValue: ", question.answers);
             };
 
             //console.log("the form: ", scope.answerEvaluationForm);
             //console.log("attributes: ", attributes);
-            console.log("question: ", scope.question);
-            /*
+            //console.log("question: ", scope.question);
+            
             scope.test = function() {
                 console.log("question ", scope.question);
             };
-            */
         }
     };
 });

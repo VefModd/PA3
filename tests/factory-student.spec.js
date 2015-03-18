7bef7970dc537a1b6c91186233a720dfdabda1fb
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-03-18 11:39:35 +0000

    posting answer

--- tests/factory-student.spec.js
+++ tests/factory-student.spec.js
@@ -2,21 +2,24 @@ describe('dispatchStudent', function() {
     beforeEach(module('angularEvaluation'));
 
     var factory,
-        url,
+        regex,
         template = "WOW SUCH FAKE";
 
     beforeEach(inject(function(dispatchStudent, _$httpBackend_, _$rootScope_, _SERVER_URL_) {
         service = dispatchStudent;
         $httpBackend = _$httpBackend_;
         $rootScope = _$rootScope_
-        url = _SERVER_URL_;
+        SERVER_URL = _SERVER_URL_;
         $rootScope.data = { Token: "FAKETOKEN"};
+        // Constructing url regex to accept correct url formats
+        regex = new RegExp(SERVER_URL +
+            "courses\/(T\-[0-9]{3}\-[a-zA-Z0-9]{4})\/[0-9]{5}\/evaluations\/[0-9]+", "");
     }));
 
     describe("retrieving student cources", function() {
         it("should return the data", function() {
 
-            $httpBackend.whenGET(url + "my/courses", function(header) {
+            $httpBackend.whenGET(SERVER_URL + "my/courses", function(header) {
                 expect(header.Authorization).toBe("Basic " + $rootScope.data.Token);
                 return true;
             }).respond(template);
@@ -31,7 +34,7 @@ describe('dispatchStudent', function() {
 
         it("should fail to retrieve the data", function() {
 
-            $httpBackend.whenGET(url + "my/courses", function(header) {
+            $httpBackend.whenGET(SERVER_URL + "my/courses", function(header) {
                 expect(header.Authorization).not.toBe("Basic WRONGTOKEN");
                 return true;
             }).respond(500, { message: "An error has occurred." });
@@ -50,7 +53,7 @@ describe('dispatchStudent', function() {
     describe("retrieving student evaluations", function() {
         it("should return the data", function() {
 
-            $httpBackend.whenGET(url + "my/evaluations", function(header) {
+            $httpBackend.whenGET(SERVER_URL + "my/evaluations", function(header) {
                 expect(header.Authorization).toBe("Basic " + $rootScope.data.Token);
                 return true;
             }).respond(template);
@@ -65,7 +68,7 @@ describe('dispatchStudent', function() {
 
         it("should fail to retrieve the data", function() {
 
-            $httpBackend.whenGET(url + "my/evaluations", function(header) {
+            $httpBackend.whenGET(SERVER_URL + "my/evaluations", function(header) {
                 expect(header.Authorization).not.toBe("Basic WRONGTOKEN");
                 return true;
             }).respond(500, { message: "An error has occurred." });
@@ -85,8 +88,6 @@ describe('dispatchStudent', function() {
 
         it("should return the data", function() {
 
-        // Constructing url regex to accept correct url formats
-        var regex = new RegExp(url + "courses\/(T\-[0-9]{3}\-[a-zA-Z0-9]{4})\/[0-9]{5}\/evaluations\/[0-9]+", "");
 
             $httpBackend.whenGET(regex, function(header) {
                 expect(header.Authorization).toBe("Basic " + $rootScope.data.Token);
@@ -103,9 +104,6 @@ describe('dispatchStudent', function() {
 
         it("should fail to retrieve the data", function() {
 
-        // Constructing url regex to accept correct url formats
-        var regex = new RegExp(url + "courses\/(T\-[0-9]{3}\-[a-zA-Z0-9]{4})\/[0-9]{5}\/evaluations\/[0-9]+", "");
-
             $httpBackend.whenGET(regex, function(header) {
                 expect(header.Authorization).not.toBe("Basic WRONGTOKEN");
                 return true;
@@ -116,6 +114,31 @@ describe('dispatchStudent', function() {
             }, function(e) {
                 // Error...
                 expect(e.status).toBe(500);
+            });
+
+            $httpBackend.flush();
+        });
+    });
+
+    describe("posting answers to server", function() {
+        it("should return ok 200", function() {
+
+            var answers = [{
+                QuestionID: 123,
+                TeacherSSN: "1234567890",
+                Value: "ANSWER"
+            }]
+
+            $httpBackend.when("POST", regex, function (postdata) {
+                var data = JSON.parse(postdata)[0];
+                expect(data.QuestionID).toBe(answers[0].QuestionID);
+                expect(data.TeacherSSN).toBe(answers[0].TeacherSSN);
+                expect(data.Value).toBe(answers[0].Value);
+                return true;
+            }).respond(200, true);
+
+            service.saveAnswer('T-101-WEPO', '20151', 1337, answers).then(function(d) {
+                expect(d).toBeTruthy();
             });
 
             $httpBackend.flush();
